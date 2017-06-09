b41fc9f56fbeef66612647605b8cbc144e93e66a
Timothy Liu <chbrahmsliu@gmail.com>
2017-06-08 21:05:42 -0400

    DOC: add example on how to run parallel func

---
+++ pdf_lib/lib_build.py
@@ -0,0 +1,14 @@
+# script to execute database building
+
+from pdf_lib.parallel_func import (save_data, learninglib_build,
+                                   map_learninglib)
+from ipyparallel import Client
+
+rc = Client()
+dview = rc[:]
+
+def run_build(cif_dir):
+    fn_list = sorted([f for f in os.listdir(cif_dir) if f.endswith('.cif')])
+    full_fn_list = list(map(lambda x: os.path.join(cif_dir, x), fn_list))
+    rv = dview.apply_async(learninglib_build, full_fn_list)
+    save_data(rv)
